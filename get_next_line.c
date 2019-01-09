26da4d77413c8457d660a45bc017908da9b2292b
Julie SEGUENI <jsegueni@e3r10p17.42.fr>
2019-01-09 18:15:05 +0100

    wrfwrifji

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,33 @@
 /*   By: jsegueni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 11:03:19 by jsegueni          #+#    #+#             */
-/*   Updated: 2019/01/09 16:19:10 by jsegueni         ###   ########.fr       */
+/*   Updated: 2019/01/09 18:14:41 by jsegueni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	check_newline(char **line, char *overflow[BUFF_SIZE])
+int	check_newline(char *overflow[BUFF_SIZE], char **line)
 {
 	int i;
 	int j;
 
-	if (*line == NULL)
+	if (*overflow == NULL)
 		return (-2);
 	i = 0;
 	j = 0;
-	while ((*line)[i])
+	while ((*overflow)[i])
 	{
-		if ((*line)[i] == '\n')
+		if ((*overflow)[i] == '\n')
 		{
-			(*line)[i] = '\0';
-			++i;
-			(*overflow) = (*line + i);
+			*line = ft_strsub(*overflow, 0, i);
+			*overflow = ft_strsub(*overflow, i, ft_strlen(*overflow));
 			return (1);
 		}
 		++i;
 	}
+	*line = ft_strdup(*overflow);
+	*overflow = ft_strdup("");
 	return (0);
 }
 
@@ -43,9 +44,9 @@ int	get_next_line(const int fd, char **line)
 
 	if (*line == NULL || fd == -1)
 		return (-1);
-	res = read(fd, *line, BUFF_SIZE);
+	res = read(fd, overflow, BUFF_SIZE);
 	if (res < 0)
 		return (-1);
-	check = check_newline(line, (char**)&overflow);
+	check = check_newline((char **)&overflow, line);
 	return (res > 0);
 }
